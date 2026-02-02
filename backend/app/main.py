17cfd87bb8643963cd9eb8c454bad91d883ed5d6
Drexter-07 <sahilsiddhant1999@gmail.com>
2026-02-03 01:21:35 +0530

    Fixed CORS issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://hrms-lite-phi-orcin.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
